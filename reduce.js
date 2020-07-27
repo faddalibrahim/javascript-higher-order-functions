67a798ab9ae7f61572c7c998dc6a4b9b561bd28f
Faddal Ibrahim <41176161+faddalibrahim@users.noreply.github.com>
2020-07-27 12:21:18 +0000

    added null coalescing if there is no third parameter is passed

--- reduce.js
+++ reduce.js
@@ -1,5 +1,5 @@
 function reduce(array, combine, start){
-    let accumulator = start;
+    let accumulator = start ?? 0;
     for(let element of array){
         accumulator = combine(accumulator, element)
         //accumulator = accumulator (+*/%) element
@@ -7,5 +7,5 @@ function reduce(array, combine, start){
     return accumulator
 }
 
-let results = reduce([1,2,3,4,5,6,7], (a, b) => a * b, 1);
+let results = reduce([1,2,3,4,5,6,7], (a, b) => a + b);
 console.log(results)
